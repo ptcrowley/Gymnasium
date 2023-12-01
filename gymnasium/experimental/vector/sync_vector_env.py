550ee20319eeed5d7ae54fae6e0c706f572fb44d
Peter Crowley <petertc@bu.edu>
2023-12-01 10:54:02 -0500

    Changing rewards array to object type

--- gymnasium/experimental/vector/sync_vector_env.py
+++ gymnasium/experimental/vector/sync_vector_env.py
@@ -69,7 +69,7 @@ class SyncVectorEnv(VectorEnv):
         self.observations = create_empty_array(
             self.single_observation_space, n=self.num_envs, fn=np.zeros
         )
-        self._rewards = np.zeros((self.num_envs,), dtype=np.float64)
+        self._rewards = np.zeros((self.num_envs,), dtype=object)
         self._terminateds = np.zeros((self.num_envs,), dtype=np.bool_)
         self._truncateds = np.zeros((self.num_envs,), dtype=np.bool_)
 
